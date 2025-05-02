e2509ba6f52100bd9ea87408d2a709ea686474df
IMStepFather <lx6688@88.com>
2025-05-03 01:11:01 +0800

    更新API地址添加端口号666

--- src/common.rs
+++ src/common.rs
@@ -956,7 +956,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://hwrd.680868.xyz".to_owned()
+    "https://hwrd.680868.xyz:666".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
