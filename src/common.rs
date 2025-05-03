296449ad0576a9f81597841c1638baffefa3d821
IMStepFather <133768437+IMStepFather@users.noreply.github.com>
2025-05-04 05:56:17 +0800

    common.rs
    地址更改为云服务器

--- src/common.rs
+++ src/common.rs
@@ -956,7 +956,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://hwrd.680868.xyz:666".to_owned()
+    "https://hwrd.bemyai.xyz".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
